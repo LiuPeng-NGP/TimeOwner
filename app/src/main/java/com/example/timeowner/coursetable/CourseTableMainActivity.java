37d5d19e19cc8b250eb5e841552b839c32bd38e5
XiangZeNan520 <105527147+XiangZeNan520@users.noreply.github.com>
2022-05-25 16:50:56 +0800

    update courseTable
    除连接数据库外，设计好了课程表的数据源

--- app/src/main/java/com/example/timeowner/coursetable/CourseTableMainActivity.java
+++ app/src/main/java/com/example/timeowner/coursetable/CourseTableMainActivity.java
@@ -5,8 +5,16 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import com.alamkanak.weekview.WeekView;
+import com.zhuangfei.timetable.TimetableView;
+import com.zhuangfei.timetable.view.WeekView;
 import com.example.timeowner.R;
+
+import java.util.List;
+
+/**
+ * 课程表主函数
+ * @author jiangx
+ */
 
 public class CourseTableMainActivity extends Activity {
 
@@ -16,5 +24,13 @@ public class CourseTableMainActivity extends Activity {
         setContentView(R.layout.concentration_main);
 
         WeekView mWeekView = (WeekView) findViewById(R.id.weekView);
+        TimetableView mTimetableView = (TimetableView) findViewById(R.id.mTimetableView);
+
+        //模拟获取课程数据：自定义格式
+        List<MySubject> mySubjects = SubjectRepertory.loadDefaultSubjects();
+        //设置数据源并显示
+        mTimetableView.source(mySubjects)
+                .curWeek(1)
+                .showView();
     }
 }
---
+++ app/src/main/java/com/example/timeowner/coursetable/MySubject.java
@@ -0,0 +1,122 @@
+package com.example.timeowner.coursetable;
+
+import com.zhuangfei.timetable.model.Schedule;
+import com.zhuangfei.timetable.model.ScheduleEnable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 课程类
+ * @author jiangx
+ */
+
+public class MySubject implements ScheduleEnable {
+
+    String name = "";//课程名
+    int day = 0;//周几上
+    String room = "";//教室
+    int start = 0;//第几周开始
+    int step = 0;//持续节数
+    String teacher = "";//老师
+    List<Integer> weekList = new ArrayList<>();//上课周表
+    int colorRandom = 0;//课程颜色
+    //Map表,包含学分、课程号、类型、学期
+    Map<String,Object> extras=new HashMap<>();
+
+    public MySubject(){}
+
+    public int getColorRandom() {//随机选取，但要保证不同
+        return colorRandom;
+    }
+
+    public void setColorRandom(int colorRandom) {
+        this.colorRandom = colorRandom;
+    }
+
+    //待补充
+    public List<Integer> getWeekList() {
+        return weekList;
+    }
+
+    public void setWeekList(List<Integer> weekList) {
+        this.weekList = weekList;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public Map<String, Object> getExtra() {
+        int credit = 0;
+        int type = 0;
+        String courseId = "";
+        String term = "";
+        extras.put("credit",credit);
+        extras.put("type",type);
+        extras.put("courseId",courseId);
+        extras.put("term",term);
+        return extras;
+    }
+
+    @Override
+    public Schedule getSchedule() {
+        Schedule schedule = new Schedule();
+        schedule.setDay(getDay());
+        schedule.setName(getName());
+        schedule.setRoom(getRoom());
+        schedule.setStart(getStart());
+        schedule.setStep(getStep());
+        schedule.setTeacher(getTeacher());
+        schedule.setWeekList(getWeekList());
+        schedule.setColorRandom(getColorRandom());
+        schedule.setExtras(getExtra());
+        return schedule;
+    }
+}
