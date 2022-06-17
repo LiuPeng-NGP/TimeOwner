85b75d35e19d38bf21c29e90ac015a8a96690a8c
XiangZeNan520 <105527147+XiangZeNan520@users.noreply.github.com>
2022-06-17 13:50:52 +0800

    channel

---
+++ app/src/main/java/com/example/timeowner/discussion/ChannelActivity.java
@@ -0,0 +1,10 @@
+package com.example.timeowner.discussion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 频道选择函数
+ * @author jiangx
+ */
+public class ChannelActivity extends AppCompatActivity {
+}
